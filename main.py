2ba7eb6f546d5fcff377bcb639d19ec64bf6b7c2
VivaceJamiel <jimpoy2@gmail.com>
2021-08-09 01:30:47 -0500

    finished but not debugged

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
+import json
+
 while True:
     file = input("Enter the filename\n")
     print("\nOpening " + file + '...\n')
     try:
-        f = open(file, "r", encoding='utf-8')
+        f = open(file)
         break
     except IOError:
         input("Error in file opening, please type correct filename")
 
+lineDict = {}
 
-    print(f)
+for line in f:
+    splitted = line.split()
+    for word in splitted:
+        if '.php' in word:
+            fileNameNum = word.split("/")
+            final = fileNameNum[-1]
+            combo = final.split(":")
+            lineNum = combo[1]
+            fileName = combo[0]
+            lineDict[lineNum] = fileName
+
+with open('lineDict.txt', 'w') as file:
+    file.write(json.dumps(lineDict))
+
+
+f.close()
