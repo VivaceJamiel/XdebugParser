6d1a14fdee7337870d049f1c3bfc1fc6353f7a0e
VivaceJamiel <jimpoy2@gmail.com>
2021-08-26 18:17:35 -0500

    fixed to include full directory

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ while True:
     file = input("Enter the filename\n")
     print("\nOpening " + file + '...\n')
     try:
-        f = open(file)
+        f = open(file,'r',encoding='ISO-8859-1')
         break
     except IOError:
-        input("Error in file opening, please type correct filename")
+        input("Error in file opening, please type correct filename\n")
 
 lineDict = {}
 
 for line in f:
     splitted = line.split()
     for word in splitted:
-        if '.php' in word:
-            fileNameNum = word.split("/")
-            final = fileNameNum[-1]
-            combo = final.split(":")
+        if '.php:' in word:
+            # print(word)
+            combo = word.split(":")
+            # print(combo)
             lineNum = combo[1]
             fileName = combo[0]
             lineDict[lineNum] = fileName
@@ -25,5 +25,6 @@ for line in f:
 with open('lineDict.txt', 'w') as file:
     file.write(json.dumps(lineDict))
 
+print('Finished')
 
 f.close()
---
+++ revised.py
@@ -0,0 +1,33 @@
+import json
+
+while True:
+    file = input("Enter the filename\n")
+    print("\nOpening " + file + '...\n')
+    try:
+        f = open(file,'r',encoding='ISO-8859-1')
+        break
+    except IOError:
+        input("Error in file opening, please type correct filename\n")
+
+lineDict = {}
+
+for line in f:
+    splitted = line.split()
+    for word in splitted:
+        if '.php:' in word:
+            # print(word)
+            combo = word.split(":")
+            # print(combo)
+            lineNum = combo[1]
+            fileName = combo[0]
+            if fileName not in lineDict:
+                lineDict[fileName] = []
+            if lineNum not in lineDict[fileName]:
+                lineDict[fileName].append(lineNum)
+
+with open('lineDict.txt', 'w') as file:
+    file.write(json.dumps(lineDict))
+
+print('Finished')
+
+f.close()
