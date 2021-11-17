4c5432ad469d67996e522b7d0ba4c412b292502a
VivaceJamiel <jimpoy2@gmail.com>
2021-11-17 11:13:55 -0600

    fixed

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import json
 
 REGEX = '[a-zA-Z0-9-_/.]*.php:[0-9]{2,4}'
 
+def get_alert_lines(filepath):
+    with open(filepath,'r',encoding='ISO-8859-1') as file:
+        content = file.read()
+    return re.findall('.*alert\(1\)', content)
+
+
 def get_file_lines(filepath):
     with open(filepath,'r',encoding='ISO-8859-1') as file:
         content = file.read()
@@ -15,6 +21,6 @@ if __name__ == '__main__':
     parser.add_argument('filepath', help='Xdebug file path')
     parser.add_argument('output', help='output file')
     args = parser.parse_args()
-    result = get_file_lines(args.filepath)
+    result = get_alert_lines(args.filepath)
     with open(args.output, 'w+') as file:
-        json.dump(result, file)
+        file.write(json.dumps(result, indent=2))
